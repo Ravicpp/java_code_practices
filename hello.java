f262362d04ee397fd4bda76c9fb467218ba6a2db
java_code_practices <ravipandey8832@gmail.com>
2024-09-12 13:28:53 +0530

    change_code

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class hello {
     public static void main(String arg[]){
         System.out.println("hii Program");
+        int a =9;
+        int b =10;
+        int c= a+b;
+        System.out.println("the sum of a + b is:"+c);
     }
 }
